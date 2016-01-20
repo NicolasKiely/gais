3e59ec2aba7867159d2f8fa365012d20b009fb99
Nic Kiely <mitchell3141@gmail.com>
2016-01-20 13:18:20 -0500

    Initial parser states

--- src/lang_syntax.c
+++ src/lang_syntax.c
@@ -1,6 +1,10 @@
+#include <stdlib.h>
 #include "lang_syntax.h"
 
 
+/*****************************************************************************\
+| Scoped Syntax Tree                                                          |
+\*****************************************************************************/
 Scope *newScope()
 {
   Scope *s = malloc(sizeof(Scope));
@@ -9,22 +13,51 @@ Scope *newScope()
 }
 
 
-Scope *parseTokens(
-    Token *token
-){
-  Scope *sTree = NULL;
-
-  /* Loop over tokens */
-  for (; token != NULL; token = token->next)
-
-  }
-
-  return sTree;
-}
-
-
 void freeScope(
     Scope *sTree
 ){
   free(sTree);
+}
+
+
+/*****************************************************************************\
+| Syntax Tree Context                                                         |
+\*****************************************************************************/
+void initializeParserContext(
+    Parser *pc
+){
+  pc->root = newScope();
+
+  /* Initial/Indeterminate state */
+  pc->states[PARST_INTR].enter = NULL;
+  pc->states[PARST_INTR].exit = NULL;
+  pc->states[PARST_INTR].read = NULL;
+  pc->states[PARST_INTR].next = parstIntrNext;
+}
+
+
+/*****************************************************************************\
+| Parser                                                                      |
+\*****************************************************************************/
+Scope *parseTokens(
+    Token *token
+){
+  Parser pc;
+
+  initializeParserContext(&pc);
+
+  /* Loop over tokens */
+  for (; token != NULL; token = token->next){
+    /* Get next parsing state for given token */
+    // ***getNextParseState()
+
+    // Error check parse state
+
+    /* Check for interstate transitions */
+
+    /* Process token */
+  }
+
+  /* Return root context */
+  return pc.root;
 }
--- src/lang_syntax.h
+++ src/lang_syntax.h
@@ -3,6 +3,11 @@
 
 #include "lang_tokenizer.h"
 
+/** Parser state indices */
+#define PARST_INTR 0
+#define PARST_COUNT 1
+
+
 /*****************************************************************************\
 | Scoped Syntax Tree                                                          |
 \*****************************************************************************/
@@ -21,13 +26,54 @@ typedef struct Scope
  */
 Scope *newScope();
 
-
 /**
  * Frees a Syntax Scope Tree
  * @param token Pointer to root token to free
  */
 void freeScope(
     Scope *sTree
+);
+
+
+/*****************************************************************************\
+| Syntax Tree Context                                                         |
+\*****************************************************************************/
+typedef struct parserContext Parser;
+typedef struct parserState ParserState;
+
+/** Parser state */
+struct parserState
+{
+  /* Callback function for entering state */
+  void (*enter)(Parser *pc);
+
+  /* Callback function for exiting state */
+  void (*exit)(Parser *pc);
+
+  /* Callback function for processing a token */
+  void (*read)(Parser *pc, Token *t);
+
+  /* Callback function for determing next token state */
+  ParserState *(*next)(Parser *pc, Token *t);
+};
+
+
+/** Parser Context */
+struct parserContext
+{
+  /* Root parsing scope */
+  Scope *root;
+
+  /* Array of parser states */
+  ParserState states[PARST_COUNT];
+};
+
+/**
+ * Initializes the parser context
+ * @param pc Parser context
+ */
+void initializeParserContext(
+    Parser *pc
 );
 
 
